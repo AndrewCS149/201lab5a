9d21be78f41ca71bc2921e4a2f55627a2e467d66
AndrewCS149 <Andrewcs149@gmail.com>
2020-05-01 22:32:33 -0700

    Complete function 4

--- starter-code/app.js
+++ starter-code/app.js
@@ -83,10 +83,24 @@ IMPORTANT DETAIL: You may not use the arithmetic operator + in this function. To
 Test this function by hand in the console to get it working, and when you think it is finished, uncomment the call for the testSumArray() function and see if the test passes.*/
 
 // Write your code here
-// var testArray = [2, 3, 4]; //eslint-disable-line
+var testArray = [2, 3, 4]; //eslint-disable-line
 
 function sumArray(sumArr) { //eslint-disable-line
 
+  // separate all values
+  var num1 = sumArr[0];
+  var num2 = sumArr[1];
+  var num3 = sumArr[2];
+
+  // add all values
+  var sum1 = sum(num1, num2);
+  var sum2 = sum(sum1[0], num3);
+
+  // create string
+  var str = `${sumArr.toString()} was passed in as an array of numbers, and ${sum2[0]} is their sum.`;
+
+  // return sum2 and str as an array
+  return [sum2[0], str];
 }
 
 // Here is the test for sumArray(); uncomment it to run it
